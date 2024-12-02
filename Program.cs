7a03cacfe5be1d34e85e1cd1f0bb3fa58bc466c9
Crossu888 <crossu999@gmail.com>
2024-12-02 23:21:41 +0100

    day 2 - failure

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System.Formats.Asn1;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
 internal class AoC
 {
     static uint[] Sort(uint[] array)
@@ -74,12 +77,134 @@ internal class AoC
         }
         Console.WriteLine(simScore);
     }
+    static void Day2()
+    {
+        StreamReader file = new StreamReader("inputs/input2test");
+        int safe=0;
+        Console.WriteLine("Part 1 or 2?");
+        string part = Console.ReadLine()!;
+        if(!(part=="1"||part=="2")) {Console.WriteLine("Invalid input"); return;}
+        for(int i=0;i<10;i++)
+        {
+            string[] report = file.ReadLine()!.Split(" ");
+            int[] levels = new int[report.Length];
+            for(int n=0;n<report.Length;n++)
+            {
+                levels[n]=Convert.ToInt32(report[n]);
+            }
+            // Check #1 - check if the report is ascending, descending or neither
+            // -1 = unsafe report | 0 = descending | 1 = ascending
+            int reportOrder = Day2check1(levels,part).order;
+            Console.WriteLine(reportOrder);
+            Console.WriteLine(Day2check1(levels,part).ignore);
+            Console.WriteLine();
+            if(reportOrder==-1) {continue;}
+            // Check #2 - check if each number is less than 4 off the previous number
+            if(Day2check2(reportOrder,levels,part,Day2check1(levels,part).ignore))
+                safe++;
+            //Console.WriteLine(Day2check2(reportOrder,levels,part,Day2check1(levels,part).ignore));
+        }
+        Console.WriteLine(safe);
+    }
+    static (int order, int ignore) Day2check1(int[] report, string part)
+    {
+        int order=0;
+        int ignoreIndex=-1;
+        bool ignored=false;
+        if(part=="1") {ignored=true;}
+        for(int i=0;i<report.Length-1;i++)
+        {
+            if(report[i]>=report[i+1] && order>0 && ignored==false) {ignoreIndex=i+1; ignored=true;}
+            else if(report[i]<=report[i+1] && order<0 && ignored==false) {ignoreIndex=i+1; ignored=true;}
+            else if(report[i]>report[i+1]) {order--;}
+            else if(report[i]<report[i+1]) {order++;}
+        }
+        //Console.WriteLine(order);
+        //Console.WriteLine(report.Length-2);
+        //Console.WriteLine();
+        if(part=="1")
+        {
+            if(order==report.Length-1)
+                return (1,-1);
+            else if(-order==report.Length-1)
+                return (0,-1);
+            else
+                return (-1,-1);
+        }
+        else if(part=="2")
+        {
+            if(order==report.Length-2||order==report.Length-1)
+                return (1,ignoreIndex);
+            else if(-order==report.Length-2||-order==report.Length-1)
+                return (0,ignoreIndex);
+            else
+                return (-1,ignoreIndex);
+        }
+        else
+            return (-1,-1);
+    }
+    static bool Day2check2(int reportOrder, int[] report, string part, int ignoreIndex)
+    {
+        if(ignoreIndex==-1 && part=="2")
+        {
+            int ignored=-1;
+            if(reportOrder==0)
+            {
+                for(int i=0;i<report.Length-1;i++)
+                {
+                    if(!(report[i+1]>=report[i]-3) && ignored!=-1)
+                        return false;
+                    else if(!(report[i+1]>=report[i]-3))
+                        ignored=i+1;
+                }
+            }
+            else if(reportOrder==1)
+            {
+                for(int i=0;i<report.Length-1;i++)
+                {
+                    //Console.WriteLine(report[i]);
+                    if(!(report[i+1]<=report[i]+3) && ignored!=-1)
+                    {
+                        return false;
+                    }
+                    else if(!(report[i+1]<=report[i]+3))
+                        ignored=i+1;
+                }
+            }
+            return true;
+        }
+        else if(reportOrder==0)
+        {
+            for(int i=0;i<report.Length-1;i++)
+            {
+                if(i!=ignoreIndex)
+                {
+                    if(!(report[i+1]>=report[i]-3))
+                        return false;
+                }
+            }
+        }
+        else if(reportOrder==1)
+        {
+            for(int i=0;i<report.Length-1;i++)
+            {
+                if(i!=ignoreIndex)
+                {
+                    if(!(report[i+1]<=report[i]+3))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
     private static void Main()
     {
         Console.WriteLine("Select the day of the puzzle");
         string day = Console.ReadLine()!;
         if(day=="1")
             Day1();
+        else if(day=="2")
+            Day2();
         else
             Console.WriteLine("Invalid input");
     }
